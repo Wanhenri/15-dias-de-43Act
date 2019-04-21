94dc9cf0801dd75b7022e5531973f6e524a34cec
wanhenri <slipkorn.jk@gmail.com>
2019-04-21 18:45:56 -0300

    modificação no SquareCollab

--- app/src/components/SquareCollab/index.js
+++ app/src/components/SquareCollab/index.js
@@ -3,7 +3,9 @@ import React from "react";
 import "./styles.css";
 
 const SquareCollab = () => (
-        <button className="square-collab">X</button>
+        <button 
+                onClick={() => console.log("FOI!") } 
+                className="square-collab">X</button>
 );
 
 export default SquareCollab;
