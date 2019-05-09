b967a03a77bb3d08b839cf1c65b982309a22a023
wanhenri <slipkorn.jk@gmail.com>
2019-05-08 21:54:39 -0300

    atualizado

--- app/src/components/SquareCollab/index.js
+++ app/src/components/SquareCollab/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { Component} from "react";
 
 import "./styles.css";
 
-const handleClick = texto => () => console.log(texto);
+class SquareCollab extends Component{
+        
+        handleClick = () => console.log("FOI!!")    
 
-const SquareCollab = () => (
-        <button 
-                onClick={ handleClick("coiso") } 
-                className="square-collab">X</button>
-);
+
+        render(){
+            return(
+                <button 
+                    onClick={ this.handleClick } 
+                    className="square-collab">X</button>
+            )
+        }
+}
+
+//const handleClick = texto => () => console.log(texto);
+//
+// const SquareCollab = () => (
+//         <button 
+//                 onClick={ handleClick("coiso") } 
+//                 className="square-collab">X</button>
+// );
+// 
 
 export default SquareCollab;
