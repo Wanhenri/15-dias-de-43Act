51c54d9c4e0a38042faeccd440c67235020c85ed
wanhenri <slipkorn.jk@gmail.com>
2019-05-11 15:07:48 -0300

    mostrado outro jeito de utilizar o map()

--- app/src/components/BoardCollab/index.js
+++ app/src/components/BoardCollab/index.js
@@ -7,6 +7,11 @@ import "./styles.css";
 const createSquares = () => {
     const squares = Array(9).fill();
 
+    // const newSquares = squares.map(function(square, index){
+    //     return <SquareCollab key={index} />
+    // });
+    // return newSquares;
+
     // console.log("valor: ", squares);
 
     return squares.map((square, index) => <SquareCollab key={index} />)
